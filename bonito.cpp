5f5dd3cdb646e8c7f7725c2353f7983e26e9a684
zico-cpu <rayhanrafi535@gmail.com>
2025-10-27 14:06:10 +0600

    app

---
+++ bonito.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the iostream library for input/output operations
+
+int main() { // The main function, where program execution begins
+    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
+    return 0; // Indicate successful program execution
+}
